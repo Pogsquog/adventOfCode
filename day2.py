12a741b28fe511317ab3cece5191a3532d11a785
Stuart Holliday <stuart.holliday@irisys.co.uk>
2025-12-07 23:56:01 +0000

    day2 part2

--- day2.py
+++ day2.py
@@ -13,7 +13,23 @@ def day2_aux(dataset):
                 result += x
     print(result)
 
+def day2_aux2(dataset):
+    result = 0
+    for item in dataset:
+        for x in range(int(item[0]), int(item[1])+1):
+            y = str(x)
+            strlen = len(y)
+            for i in range(2, strlen + 1):
+                if strlen % i != 0: # can't split in this many
+                    continue
+                if y[0:strlen//i]*i == y:
+                    if i != 2:
+                        print(y)
+                    result += x
+                    break
+    print(result)
+
 def day2():
     dataset = load_data2('data/day2')
-    day2_aux(dataset)
+    day2_aux2(dataset)
 
