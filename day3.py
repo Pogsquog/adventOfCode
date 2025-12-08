952aa843d98cb02e91a62767edb0377e1f6c29c4
Stuart Holliday <stuart.holliday@irisys.co.uk>
2025-12-08 00:25:40 +0000

    day3 part1

---
+++ day3.py
@@ -0,0 +1,32 @@
+from shared import load_data1
+
+
+def day3_aux(dataset):
+    result = 0
+    for battery in dataset:
+        # find the highest first digit
+        high1 = -1
+        high1_index = 0
+        for i in range(len(battery)-1):
+            if int(battery[i]) > high1:
+                high1 = int(battery[i])
+                high1_index = i
+        high2 = -1
+        high2_index = 0
+
+        # find the highest second digit after the highest first digit
+        for i in range(high1_index+1, len(battery)):
+            if int(battery[i]) > high2:
+                high2 = int(battery[i])
+                high2_index = i
+
+        jolts = high1 * 10 + high2
+        print(jolts, high1_index, high2)
+        result += jolts
+
+    print(result)
+
+
+def day3():
+    dataset = load_data1('data/day3')
+    day3_aux(dataset)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from day1 import day1
 from day2 import day2
+from day3 import day3
 
 if __name__ == '__main__':
-    day2()
+    day3()
