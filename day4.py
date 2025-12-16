9c6c649afb9664cae7735d41f2fc8a94bc2a57de
Stuart Holliday <stuart.holliday@irisys.co.uk>
2025-12-16 23:36:16 +0000

    day 4 part1 done

---
+++ day4.py
@@ -0,0 +1,33 @@
+from shared import load_data1
+from typing import List
+
+
+def count_neighbours(dataset, y, x, y_size, x_size):
+    y_min = 0 if y == 0 else y - 1
+    x_min = 0 if x == 0 else x - 1
+    y_max = y if y + 1 == y_size else y + 1
+    x_max = x if x + 1 == x_size else x + 1
+
+    total = 0
+    for row in dataset[y_min:y_max + 1]:
+        for column in row[x_min:x_max + 1]:
+            if column == '@':
+                total += 1
+
+    return total - 1  # subtract one for this square
+
+def day4_aux(dataset):
+    y_size = len(dataset)
+    x_size = len(dataset[0])
+
+    num_removable = 0
+    for y in range(0, y_size):
+        for x in range(0, x_size):
+            if dataset[y][x] == '@' and count_neighbours(dataset, y, x, y_size, x_size) < 4:
+                num_removable += 1
+    print(num_removable)
+
+
+def day4():
+    dataset = load_data1('data/day4')
+    day4_aux(dataset)
--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import time
 from day1 import day1
 from day2 import day2
 from day3 import day3
+from day4 import day4
 
 if __name__ == '__main__':
     start_time = time.time()
-    day3()
+    # day1()
+    # day2()
+    # day3()
+    day4()
     end_time = time.time()
 
     elapsed_time = end_time - start_time
